f73d376f53ee6e46b83a61a057057190e4001fd0
Azin A <aalahkhani@gmail.com>
2023-11-09 16:25:55 +0000

    add some logic for pressure plates

--- Assets/PressurePlate.cs
+++ Assets/PressurePlate.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+    public Animator cubeAnimator;
+    private bool activated = false;
+
+    private void Start() {
+        cubeAnimator = GameObject.Find("Cube(5)").GetComponent<Animator>();
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.collider)
         {
             Debug.Log("Pressed");
+            activated = true;
+            cubeAnimator.SetTrigger("SlideUp");
         }
     }
 
     private void OnCollisionExit(Collision other)
     {
-        if (other.collider)
+        if (other.collider && activated)
         {
+            activated = false;
             Debug.Log("Unpressed");
+            cubeAnimator.SetTrigger("SlideDown");
         }
     }
 }
