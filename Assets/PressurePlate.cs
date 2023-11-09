5f7fb4d9db4d15af2f0235d3c57584298cc91ae5
Azin A <aalahkhani@gmail.com>
2023-11-09 17:40:56 +0000

    fixed bug

--- Assets/PressurePlate.cs
+++ Assets/PressurePlate.cs
@@ -6,28 +6,37 @@ public class PressurePlate : MonoBehaviour
 {
     public Animator cubeAnimator;
     private bool activated = false;
+    private bool paused = false;
 
     private void Start() {
         cubeAnimator = GameObject.Find("Cube(5)").GetComponent<Animator>();
     }
+    private IEnumerator Pause(string name){
+        paused = true;
+        yield return new WaitForSeconds(1);
+        cubeAnimator.ResetTrigger(name);
+        paused = false;
+    }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.collider)
+        if (other.collider && !activated && !paused)
         {
             Debug.Log("Pressed");
             activated = true;
             cubeAnimator.SetTrigger("SlideUp");
+            StartCoroutine(Pause("SlideUp"));
         }
     }
 
     private void OnCollisionExit(Collision other)
     {
-        if (other.collider && activated)
+        if (other.collider && activated && !paused)
         {
             activated = false;
             Debug.Log("Unpressed");
             cubeAnimator.SetTrigger("SlideDown");
+            StartCoroutine(Pause("SlideDown"));
         }
     }
 }
